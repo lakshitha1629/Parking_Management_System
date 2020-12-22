25c6fe15818a9a165ab159a6610fdd6f90735d2e
lakshitha1629 <lakshitha1629@gmail.com>
2020-12-23 01:46:30 +0530

    fix bugs

--- customer_register.php
+++ customer_register.php
@@ -1,3 +1,49 @@
+<?php
+if (isset($_POST['reg_btn'])) {
+    require_once('connect.php');
+
+    date_default_timezone_set('Asia/Colombo');
+    $date = date('Y-m-d H:i:s');
+
+    //$date = $_POST['date'];
+    $user_id = uniqid();
+    $name = $_POST['FirstName'] . ' ' . $_POST['LastName'];
+    $VehicleNumberPlate = $_POST['VehicleNumberPlate'];
+    $VehicleTypes = $_POST['VehicleTypes'];
+    $PhoneNumber = $_POST['PhoneNumber'];
+    $Email = $_POST['Email'];
+    $password1 = $_POST['Password'];
+    $password2 = $_POST['RepeatPassword'];
+    $type = '2';
+    $active = '1';
+
+    // QR Code
+    include './phpqrcode/qrlib.php';
+    $path = 'images/QR/';
+    $QR_Code = $path . $user_id . ".png";
+    $ecc = 'L';
+    $pixel_Size = 10;
+    $frame_Size = 10;
+
+    // Generates QR Code and Stores it in directory given 
+    QRcode::png($user_id, $QR_Code, $ecc, $pixel_Size);
+
+    if ($password1 != $password2) {
+        echo "The two passwords do not match";
+    } else {
+        $password = md5($password1);
+        $qry = "INSERT INTO `user_account`(`user_id`,`name`, `email`, `user_type`, `password`, `activated`, `number_plate`, `vehicle_type`, `phone`) VALUES ('$user_id','$name','$Email','$type','$password','$active','$VehicleNumberPlate','$VehicleTypes','$PhoneNumber')";
+        $qry1 = "INSERT INTO `smart_wallet`(`date`, `email`, `price`) VALUES ('$date', '$Email', '1000')";
+
+        mysqli_query($con, $qry);
+        $result = mysqli_query($con, $qry1)
+            or die('Error: ' . mysqli_error($con));
+
+        header("location: login.php");
+        // echo "Registration Successfull";
+    }
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -71,55 +117,7 @@
                                 <input class="btn btn-primary btn-user btn-block" id="reg_submit" name="reg_btn" type="submit" value="Register Account" />
 
                             </form>
-                            <?php
-                            if (isset($_POST['reg_btn'])) {
-                                require_once('connect.php');
 
-                                date_default_timezone_set('Asia/Colombo');
-                                $date = date('Y-m-d H:i:s');
-
-                                //$date = $_POST['date'];
-                                $user_id = uniqid();
-                                $name = $_POST['FirstName'] . ' ' . $_POST['LastName'];
-                                $VehicleNumberPlate = $_POST['VehicleNumberPlate'];
-                                $VehicleTypes = $_POST['VehicleTypes'];
-                                $PhoneNumber = $_POST['PhoneNumber'];
-                                $Email = $_POST['Email'];
-                                $password1 = $_POST['Password'];
-                                $password2 = $_POST['RepeatPassword'];
-                                $type = '2';
-                                $active = '1';
-
-                                // QR Code
-                                include './phpqrcode/qrlib.php';
-                                $path = 'images/QR/';
-                                $QR_Code = $path . $user_id . ".png";
-                                $ecc = 'L';
-                                $pixel_Size = 10;
-                                $frame_Size = 10;
-
-                                // Generates QR Code and Stores it in directory given 
-                                QRcode::png($user_id, $QR_Code, $ecc, $pixel_Size);
-
-                                if ($password1 != $password2) {
-                                    echo "The two passwords do not match";
-                                } else {
-                                    $password = md5($password1);
-                                    $qry = "INSERT INTO `user_account`(`user_id`,`name`, `email`, `user_type`, `password`, `activated`, `number_plate`, `vehicle_type`, `phone`) VALUES ('$user_id','$name','$Email','$type','$password','$active','$VehicleNumberPlate','$VehicleTypes','$PhoneNumber')";
-                                    $qry1 = "INSERT INTO `smart_wallet`(`date`, `email`, `price`) VALUES ('$date', '$Email', '1000')";
-
-                                    mysqli_query($con, $qry1);
-                                    $result = mysqli_query($con, $qry)
-                                        or die('Error: ' . mysqli_error($con));
-
-                                    // echo "Registration Successfull";
-                                    echo "<script type='text/javascript'>";
-                                    echo "alert('Registration Successfull');";
-                                    echo 'window.location.href = "index.php";';
-                                    echo "</script>";
-                                }
-                            }
-                            ?>
                             <hr>
                             <div class="text-center">
                                 <a class="small" href="forgot-password.php">Forgot Password?</a>
